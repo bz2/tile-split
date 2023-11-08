8062bbe380ac64dc7a549d589caa65a56e5f7a8b
mariya-gfx <mariya.popova@p3-group.com>
2023-11-08 19:18:33 +0200

    Read env vars and pass args to config
    Update the Config struct with the appropriate values accordig to the
    vars and args that we pass to the shell script.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use std::env;
 use tile_split::{Config, tile_image};
 
 // what is main() returning?
 fn main() {
+    let filename: String = env::args().collect::<Vec<String>>()[0].clone();
+    let folder: String = env::var("OUTDIR").unwrap();
+    let format: String = env::var("FMT").unwrap();
+    let zoomlevel: u8 = env::var("ZOOMLEVEL").unwrap().parse::<u8>().unwrap();
 
-    // read $1
-    // read envvars
-    // make Config
-    let config = Config {
+    let config: Config = Config {
+        filename: filename.to_string(),
+        folder,
+        format,
         tilesize: 256,
-        filename: "test.png".to_string(),
-        zoomlevel: 5,
-        folder: "out".to_string(),
+        zoomlevel,
     };
 
     tile_image(config).unwrap();
