1c06c246bb66846a8fb697b2d14b9d9654ea5f17
Martin Packman <martin.packman@visual-meaning.com>
2023-11-09 15:30:31 +0000

    Fix and tweak
    Dash instead of underscore for output filenames.
    
    Y-first iteration (doesn't really matter).
    
    Omit binary feature for oxipng dependencies.
    
    Tweak default oxipng options.

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
 pub struct Config<'a> {
     pub filename: &'a str, // $1
-    pub tilesize: u32,    // 256
-    pub zoomlevel: u8,    // eg 0 - 5
+    pub tilesize: u32,     // 256
+    pub zoomlevel: u8,     // eg 0 - 5
     // don't need MGK
     pub folder: &'a str, // $OUTDIR out
 }
--- src/image.rs
+++ src/image.rs
@@ -1,21 +1,31 @@
-use std::fs::File;
-use std::io::Write;
 use crate::{Config, Error};
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, GenericImageView};
+use std::fs::File;
+use std::io::Write;
 
 fn save_view(img: &DynamicImage, x: u32, y: u32, config: &Config) -> Result<(), Error> {
     let x1 = x * config.tilesize;
     let y1 = y * config.tilesize;
     let sub = img.view(x1, y1, config.tilesize, config.tilesize);
-    let path = format!("{p}/{z}_{x}_{y}.png", p=config.folder, z=config.zoomlevel, x = x, y = y);
+    let path = format!(
+        "{p}/{z}-{x}-{y}.png",
+        p = config.folder,
+        z = config.zoomlevel,
+        x = x,
+        y = y
+    );
     let png = oxipng::RawImage::new(
         config.tilesize,
         config.tilesize,
         oxipng::ColorType::RGBA,
         oxipng::BitDepth::Eight,
-        sub.to_image().into_raw()
-    )?.create_optimized_png(&oxipng::Options::default())?;
+        sub.to_image().into_raw(),
+    )?
+    .create_optimized_png(&oxipng::Options {
+        deflate: oxipng::Deflaters::Libdeflater { compression: 10 },
+        ..Default::default()
+    })?;
     let mut file = File::create(path)?;
     file.write_all(&png)?;
 
@@ -26,8 +36,8 @@ pub fn tile_image(config: Config) -> Result<(), Error> {
     let img = ImageReader::open(config.filename)?.decode()?;
 
     // Needs to be a loop, for each possible tile in the image
-    for x in 0..(img.width() / config.tilesize) {
-        for y in 0..(img.height() / config.tilesize) {
+    for y in 0..(img.height() / config.tilesize) {
+        for x in 0..(img.width() / config.tilesize) {
             save_view(&img, x, y, &config)?;
         }
     }
