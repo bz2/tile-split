8bcbfa66f977072d3e5cde26b63da042851b73e7
mariya-gfx <mariya.popova@p3-group.com>
2023-11-09 17:57:21 +0200

    @bz2, feedback changes. I agree we need to handle
    the case if there is no filename as cmd arg. I am using match (like if) and get(1),
    get returns `Option` type, if there is no arg - None, we print `stderr`
    with `eprintln!` (also specific to stderr) and exit.
    The rest is a cleanup from feedback.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,20 @@ use tile_split::{Config, tile_image};
 
 // what is main() returning?
 fn main() {
-    let filename: String = env::args().collect::<Vec<String>>()[0].clone();
-    let folder: String = env::var("OUTDIR").unwrap();
-    let format: String = env::var("FMT").unwrap();
-    let zoomlevel: u8 = env::var("ZOOMLEVEL").unwrap().parse::<u8>().unwrap();
+    // Exit with an error if there is no filename arg
+    let filename: String = match env::args().collect::<Vec<String>>().get(1) {
+        Some(arg) => arg.clone(),
+        None => {
+            eprintln!("Error: Please provide a filename.");
+            std::process::exit(1);
+        }
+    };
+    let folder: String = env::var("OUTDIR").unwrap_or("out".to_string());
+    let format: String = env::var("FMT").unwrap_or("png".to_string());
+    let zoomlevel: u8 = env::var("ZOOMLEVEL").unwrap().parse::<u8>().unwrap_or(5);
 
     let config: Config = Config {
-        filename: filename.to_string(),
+        filename,
         folder,
         format,
         tilesize: 256,
