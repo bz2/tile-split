c35adb3452448e4d1fd1d312e732c2e7ce1a03ae
Martin Packman <martin.packman@visual-meaning.com>
2023-11-09 21:17:47 +0000

    Slightly nuts

--- src/image.rs
+++ src/image.rs
@@ -1,13 +1,19 @@
 use crate::{Config, Error};
-use image::io::Reader as ImageReader;
-use image::{DynamicImage, GenericImageView};
+use fast_image_resize::{CropBox, Image, PixelType, Resizer, ResizeAlg};
+use oxipng::internal_tests::{PngData};
 use std::fs::File;
 use std::io::Write;
+use std::num::NonZeroU32;
 
-fn save_view(img: &DynamicImage, x: u32, y: u32, config: &Config) -> Result<(), Error> {
+fn save_view(img: &Image, x: u32, y: u32, config: &Config) -> Result<(), Error> {
     let x1 = x * config.tilesize;
     let y1 = y * config.tilesize;
-    let sub = img.view(x1, y1, config.tilesize, config.tilesize);
+    let mut view = img.view();
+    let size = NonZeroU32::new(config.tilesize).ok_or("zero size")?;
+    view.set_crop_box(CropBox {left: x1, top: y1, width: size, height: size})?;
+    let mut sub = Image::new(size, size, view.pixel_type());
+    let mut resizer = Resizer::new(ResizeAlg::default());
+    resizer.resize(&view, &mut sub.view_mut())?;
     let path = format!(
         "{p}/{z}-{x}-{y}.png",
         p = config.folder,
@@ -20,7 +26,7 @@ fn save_view(img: &DynamicImage, x: u32, y: u32, config: &Config) -> Result<(), 
         config.tilesize,
         oxipng::ColorType::RGBA,
         oxipng::BitDepth::Eight,
-        sub.to_image().into_raw(),
+        sub.into_vec(),
     )?
     .create_optimized_png(&oxipng::Options {
         deflate: oxipng::Deflaters::Libdeflater { compression: 10 },
@@ -33,11 +39,22 @@ fn save_view(img: &DynamicImage, x: u32, y: u32, config: &Config) -> Result<(), 
 }
 
 pub fn tile_image(config: Config) -> Result<(), Error> {
-    let img = ImageReader::open(config.filename)?.decode()?;
+    let png = std::sync::Arc::into_inner(PngData::new(std::path::Path::new(config.filename), &oxipng::Options::default() )?.raw).unwrap();
+
+    let img = fast_image_resize::Image::from_vec_u8(
+        NonZeroU32::new(png.ihdr.width).ok_or("zero width")?,
+        NonZeroU32::new(png.ihdr.height).ok_or("zero height")?,
+        png.data,
+        match png.ihdr.bpp() {
+            24 => PixelType::U8x3,
+            32 => PixelType::U8x4,
+            n => return Err(format!("unknown bits per pixel: {}", n).into()),
+        }
+    )?;
 
     // Needs to be a loop, for each possible tile in the image
-    for y in 0..(img.height() / config.tilesize) {
-        for x in 0..(img.width() / config.tilesize) {
+    for y in 0..(png.ihdr.height / config.tilesize) {
+        for x in 0..(png.ihdr.width / config.tilesize) {
             save_view(&img, x, y, &config)?;
         }
     }
