571324621a7add55c8459977d9dd86439e11c131
Mariya Popova <52455670+mariya-gfx@users.noreply.github.com>
2023-11-10 15:08:54 +0200

    Read env vars and pass args to config (#1)
    * Read env vars and pass args to config
    
    Update the Config struct with the appropriate values accordig to the
    vars and args that we pass to the shell script.
    
    * @bz2, feedback changes. I agree we need to handle
    
    the case if there is no filename as cmd arg. I am using match (like if) and get(1),
    get returns `Option` type, if there is no arg - None, we print `stderr`
    with `eprintln!` (also specific to stderr) and exit.
    The rest is a cleanup from feedback.
    
    * @bz2, thank you for the useful feedback. Changes applied.

--- src/config.rs
+++ src/config.rs
@@ -2,8 +2,8 @@
 
 pub struct Config {
     pub filename: String, // $1
+    pub folder: String, // $OUTDIR out
+    pub format: String, // png
     pub tilesize: u32, // 256
     pub zoomlevel: u8, // eg 0 - 5
-    // don't need MGK
-    pub folder: String, // $OUTDIR out
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+use std::env;
 use tile_split::{Config, tile_image};
 
 // what is main() returning?
 fn main() {
+    // Exit with an error if there is no filename arg
+    let filename: String = match env::args().nth(1) {
+        Some(arg) => arg.to_string(),
+        None => {
+            eprintln!("Error: Please provide a filename.");
+            std::process::exit(1);
+        }
+    };
+    let folder: String = env::var("OUTDIR").unwrap_or("out".to_string());
+    let format: String = env::var("FMT").unwrap_or("png".to_string());
+    let zoomlevel: u8 = env::var("ZOOMLEVEL").map(|s| s.parse::<u8>()).unwrap_or(Ok(5)).unwrap_or(5);
 
-    // read $1
-    // read envvars
-    // make Config
-    let config = Config {
+    let config: Config = Config {
+        filename,
+        folder,
+        format,
         tilesize: 256,
-        filename: "test.png".to_string(),
-        zoomlevel: 5,
-        folder: "out".to_string(),
+        zoomlevel,
     };
 
     tile_image(config).unwrap();
