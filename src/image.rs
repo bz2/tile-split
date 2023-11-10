be5c15acdfe8732bde4023a6d0e3bb0c9dd50ce8
Martin Packman <martin.packman@visual-meaning.com>
2023-11-10 11:03:07 +0000

    Resizing down is fine

--- src/image.rs
+++ src/image.rs
@@ -6,31 +6,36 @@ use std::io::Write;
 use std::num::NonZeroU32;
 
 fn save_view(img: &Image, x: u32, y: u32, config: &Config) -> Result<(), Error> {
-    let x1 = x * config.tilesize;
-    let y1 = y * config.tilesize;
-    let mut view = img.view();
-    let size = NonZeroU32::new(config.tilesize).ok_or("zero size")?;
-    view.set_crop_box(CropBox {left: x1, top: y1, width: size, height: size})?;
-    let mut sub = Image::new(size, size, view.pixel_type());
-    let mut resizer = Resizer::new(ResizeAlg::default());
-    resizer.resize(&view, &mut sub.view_mut())?;
+    let input_tilesize = 2048;
+    let x1 = x * input_tilesize;
+    let y1 = y * input_tilesize;
+    let pixel_data = {
+        let mut view = img.view();
+        let in_size = NonZeroU32::new(input_tilesize).ok_or("zero size")?;
+        let size = NonZeroU32::new(config.tilesize).ok_or("zero size")?;
+        view.set_crop_box(CropBox {left: x1, top: y1, width: in_size, height: in_size})?;
+        let mut sub = Image::new(size, size, view.pixel_type());
+        let mut resizer = Resizer::new(ResizeAlg::default());
+        resizer.resize(&view, &mut sub.view_mut())?;
+        sub.into_vec()
+    };
     let path = format!(
         "{p}/{z}-{x}-{y}.png",
         p = config.folder,
-        z = config.zoomlevel,
+        z = config.zoomlevel - 3,
         x = x,
         y = y
     );
     let png = oxipng::RawImage::new(
         config.tilesize,
         config.tilesize,
-        match view.pixel_type() {
+        match img.pixel_type() {
             PixelType::U8x3 => oxipng::ColorType::RGB { transparent_color: None },
             PixelType::U8x4 => oxipng::ColorType::RGBA,
             t => return Err(format!("unknown pixel type: {:?}", t).into()),
         },
         oxipng::BitDepth::Eight,
-        sub.into_vec(),
+        pixel_data,
     )?
     .create_optimized_png(&oxipng::Options {
         deflate: oxipng::Deflaters::Libdeflater { compression: 10 },
@@ -56,9 +61,10 @@ pub fn tile_image(config: Config) -> Result<(), Error> {
         }
     )?;
 
+    let input_tilesize = 2048;
     // Needs to be a loop, for each possible tile in the image
-    for y in 0..(png.ihdr.height / config.tilesize) {
-        for x in 0..(png.ihdr.width / config.tilesize) {
+    for y in 0..(png.ihdr.height / input_tilesize) {
+        for x in 0..(png.ihdr.width / input_tilesize) {
             save_view(&img, x, y, &config)?;
         }
     }
