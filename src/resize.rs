3427c3d5b1eb5e81e1a467f49be4ed0e669d43a2
Martin Packman <martin.packman@visual-meaning.com>
2023-11-13 21:42:59 +0000

    More typed like this trait

--- src/resize.rs
+++ src/resize.rs
@@ -2,55 +2,43 @@ use crate::Config;
 use image::imageops;
 use image::DynamicImage;
 
-pub type ResizedItem = (DynamicImage, u8);
-
-pub struct Resizer<'c> {
-    pub config: &'c Config<'c>,
+pub trait Resizer<T> {
+    fn resize_range(&self, img: &T) -> Vec<(T, u8)>;
 }
 
-impl<'c> Resizer<'c> {
-    fn _check_dimension(&self, img: &'c DynamicImage) {
-        let (img_width, img_height) = (img.width(), img.height());
-        let max_dimension_size = self.config.tilesize << self.config.zoomlevel;
-        if img_width != max_dimension_size || img_height != max_dimension_size {
-            panic!(
-                "Image of size {w}x{h} cannot be split into
-                tiles of size {tile_size} and max zoom level {max_zoom}.",
-                w = img_width,
-                h = img_height,
-                tile_size = self.config.tilesize,
-                max_zoom = self.config.zoomlevel,
-            );
-        }
+type ResizedItem = (DynamicImage, u8);
+
+fn _check_dimension(config: &Config, img: &DynamicImage) {
+    if config.zoomrange.end() > &config.zoomlevel {
+        panic!("Zoom range has value(s) larger than zoom level.");
     }
-
-    fn _resize(img: &'c DynamicImage, width: u32, height: u32) -> DynamicImage {
-        img.resize(width, height, imageops::FilterType::Lanczos3)
+    let (img_width, img_height) = (img.width(), img.height());
+    let max_dimension_size = config.tilesize << config.zoomlevel;
+    if img_width != max_dimension_size || img_height != max_dimension_size {
+        panic!(
+            "Image of size {w}x{h} cannot be split into
+            tiles of size {tile_size} and max zoom level {max_zoom}.",
+            w = img_width,
+            h = img_height,
+            tile_size = config.tilesize,
+            max_zoom = config.zoomlevel,
+        );
     }
+}
 
-    pub fn new(config: &'c Config<'c>) -> Self {
-        let Config {
-            zoomlevel,
-            zoomrange,
-            ..
-        } = &config;
-        // Do we need zoomlevel?
-        if zoomrange.iter().any(|x| x > zoomlevel) {
-            panic!("Zoom range has value(s) larger than zoom level.");
-        }
+fn _resize(img: &DynamicImage, width: u32, height: u32) -> DynamicImage {
+    img.resize(width, height, imageops::FilterType::Lanczos3)
+}
 
-        Self { config }
-    }
+impl Resizer<DynamicImage> for Config<'_> {
+    fn resize_range(&self, img: &DynamicImage) -> Vec<ResizedItem> {
+        _check_dimension(self, img);
 
-    pub fn resize_range(&self, img: &'c DynamicImage) -> Vec<ResizedItem> {
-        self._check_dimension(img);
-
-        self.config
-            .zoomrange
-            .iter()
-            .map(|&x| {
-                let t_size = self.config.tilesize << x;
-                (Self::_resize(img, t_size, t_size), x)
+        self.zoomrange
+            .clone()
+            .map(|x| {
+                let t_size = self.tilesize << x;
+                (_resize(img, t_size, t_size), x)
             })
             .collect()
     }
