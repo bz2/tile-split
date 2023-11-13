50b1635c786aa2c046134fbef875da8ee626be18
Martin Packman <martin.packman@visual-meaning.com>
2023-11-13 19:41:35 +0000

    Fix fmt and clippy lints

--- src/resize.rs
+++ src/resize.rs
@@ -1,25 +1,25 @@
+use crate::Config;
 use image::imageops;
 use image::DynamicImage;
-use crate::Config;
 
 pub type ResizedItem = (DynamicImage, u8);
 
 pub struct Resizer<'c> {
-    pub config: &'c Config<'c>
+    pub config: &'c Config<'c>,
 }
-
 
 impl<'c> Resizer<'c> {
     fn _check_dimension(&self, img: &'c DynamicImage) {
         let (img_width, img_height) = (img.width(), img.height());
         let max_dimension_size = self.config.tilesize << self.config.zoomlevel;
         if img_width != max_dimension_size || img_height != max_dimension_size {
-            panic!("Image of size {w}x{h} cannot be split into
+            panic!(
+                "Image of size {w}x{h} cannot be split into
                 tiles of size {tile_size} and max zoom level {max_zoom}.",
-                w=img_width,
-                h=img_height,
-                tile_size=self.config.tilesize,
-                max_zoom=self.config.zoomlevel,
+                w = img_width,
+                h = img_height,
+                tile_size = self.config.tilesize,
+                max_zoom = self.config.zoomlevel,
             );
         }
     }
@@ -29,23 +29,29 @@ impl<'c> Resizer<'c> {
     }
 
     pub fn new(config: &'c Config<'c>) -> Self {
-        let Config{zoomlevel, zoomrange, ..} = &config;
+        let Config {
+            zoomlevel,
+            zoomrange,
+            ..
+        } = &config;
         // Do we need zoomlevel?
         if zoomrange.iter().any(|x| x > zoomlevel) {
             panic!("Zoom range has value(s) larger than zoom level.");
         }
 
-        Self {
-            config
-        }
+        Self { config }
     }
 
     pub fn resize_range(&self, img: &'c DynamicImage) -> Vec<ResizedItem> {
         self._check_dimension(img);
-    
-        self.config.zoomrange.iter().map(|&x| {
-            let t_size = self.config.tilesize << x;
-            (Self::_resize(img, t_size, t_size), x)
-        }).collect()
+
+        self.config
+            .zoomrange
+            .iter()
+            .map(|&x| {
+                let t_size = self.config.tilesize << x;
+                (Self::_resize(img, t_size, t_size), x)
+            })
+            .collect()
     }
 }
