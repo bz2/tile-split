be0ee9c94ba084b3d0c7fb624c40e2cbe4268b4e
Martin Packman <martin.packman@visual-meaning.com>
2023-11-28 16:54:51 +0000

    Parsing testing

--- src/main_tests.rs
+++ src/main_tests.rs
@@ -1,5 +1,20 @@
 use super::*;
 
+#[test]
+fn arg_parse_range_empty() {
+    for s in ["", "  ", "4-"] {
+        let err = parse_range::<u8>(s).unwrap_err();
+        assert_eq!(err.to_string(), "cannot parse integer from empty string");
+    }
+}
+
+#[test]
+fn arg_parse_range_ok() {
+    assert_eq!(parse_range::<u8>("0").unwrap(), 0..=0);
+    assert_eq!(parse_range::<u8>("3").unwrap(), 3..=3);
+    assert_eq!(parse_range::<u8>("-4").unwrap(), 0..=4);
+}
+
 /// Parse args and panic on failure but without exiting the process
 fn parse<'a, I>(arr: I) -> Args
 where
